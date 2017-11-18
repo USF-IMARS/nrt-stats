6acd4552606fe0539dcbe3894c6239ce3bdd5959
Tylar <murray.tylar@gmail.com>
2017-11-18 21:40:10 +0000

    again, but with more "e"

--- last_delays_to_graphite.sh
+++ last_delays_to_graphite.sh
@@ -7,4 +7,4 @@ last_file="$(/opt/nrt-stats/get_last_file.sh level0 *.PDS)"
 last_delay="$(/opt/nrt-stats/get_delay_for.py $last_file)"
 
        echo "$HOSTNAME.exec.ipopp.delay.l0 $last_delay `date +%s`"
-timout 3 echo "$HOSTNAME.exec.ipopp.delay.l0 $last_delay `date +%s`" | nc $SERVER $PORT
+timeout 3 echo "$HOSTNAME.exec.ipopp.delay.l0 $last_delay `date +%s`" | nc $SERVER $PORT
